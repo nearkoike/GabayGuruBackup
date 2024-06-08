d894cb7487c5d199567ef9d0dbeae7aec07917bf
Near Koike <nearkoike@gmail.com>
2024-06-08 12:20:00 +0800

    nye

--- gabayguru-client/src/firebase/firebase.config.js
+++ gabayguru-client/src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyBzAXq9X4td5oEq4bmgSG7HsqRoEiD1czE",
+  authDomain: "gabayguru-client.firebaseapp.com",
+  projectId: "gabayguru-client",
+  storageBucket: "gabayguru-client.appspot.com",
+  messagingSenderId: "817775231843",
+  appId: "1:817775231843:web:9998a0bbe445e0cb228956"
 };
 
 const app = initializeApp(firebaseConfig);
